04b4777dfe07da3d27c74d99382595b8050d9250
Aleksey <114165975+kuschneroff@users.noreply.github.com>
2023-12-25 23:06:49 +0300

    Update task_#10.py

--- HW/task_#10/task_#10.py
+++ HW/task_#10/task_#10.py
@@ -1,1 +1,11 @@
+import pandas as pd
 
+data = pd.read_csv('Pokemon.csv')  # Загрузка данных из файла 'Pokemon.csv'
+
+# Создание столбцов-индикаторов для столбца 'Type 1'
+indicator_columns = pd.get_dummies(data['Type 1'], prefix='Type')
+
+# Объединение столбцов-индикаторов с исходным DataFrame
+data_encoded = pd.concat([data, indicator_columns], axis=1)
+
+print(data_encoded)
