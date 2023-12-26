3cc97877af1eeb00a1e55f70b060f1cf77a96e91
Aleksey <114165975+kuschneroff@users.noreply.github.com>
2023-12-26 22:01:48 +0300

    Update task_#10.py

--- HW/task_#10/task_#10.py
+++ HW/task_#10/task_#10.py
@@ -1,11 +1,12 @@
 import pandas as pd
-
-data = pd.read_csv('Pokemon.csv')  # Загрузка данных из файла 'Pokemon.csv'
-
-# Создание столбцов-индикаторов для столбца 'Type 1'
-indicator_columns = pd.get_dummies(data['Type 1'], prefix='Type')
-
-# Объединение столбцов-индикаторов с исходным DataFrame
-data_encoded = pd.concat([data, indicator_columns], axis=1)
-
-print(data_encoded)
+import numpy as np
+import random
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI': lst})
+print(data)
+data['tmp'] = 1
+data.set_index([data.index, 'whoAmI'], inplace=True)
+data = data.unstack(level=-1, fill_value=0).astype(int)
+data.columns.name = None
